c18d80eaa7bd055f35a28c2381a75a6196daa565
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-04-25 16:12:01 -0300

    Agrupados campos nos formulário de casos do Django-admin

--- cases/admin.py
+++ cases/admin.py
@@ -5,3 +5,8 @@ from .models import AcumuladoDiario
 @admin.register(AcumuladoDiario)
 class AcumuladoDiarioAdmin(admin.ModelAdmin):
     list_filter = ['data']
+    fields = ('data',
+              ('casosConfirmados', 'casosDescartados',),
+              ( 'casosAguardandoResultado', 'casosSuspeitosInternados',),
+              ('obitosConfirmados', 'obitosSuspeitos'),
+              )
